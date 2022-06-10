38f06cc33ed7dc7c354a24203dec351e96025867
NyanKaungSet <96227457+NyanKaungSet@users.noreply.github.com>
2022-06-10 20:52:04 +0630

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var song = document.getElementById('song')
 var buttonContainer = document.getElementById('button_container')
-function RickTime(){
+function RickTime(){ /* Play the song when you click the button */
     song.play();
 }
 
-$(document).ready(function(){
+$(document).ready(function(){ /* Hide the button and show the Rickroll Gif */
     $("#RickBG").hide();
     $("#button").click(function(){
       $("#button_container").hide();
